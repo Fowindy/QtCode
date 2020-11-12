34cc5d44f6eba583af0576030353807bca426cbb
Fowindy <47002339@qq.com>
2020-11-12 19:24:10 +0800

    为button指定父对象

--- 02_HelloQt/main.cpp
+++ 02_HelloQt/main.cpp
@@ -7,11 +7,20 @@ int main(int argc,char **argv)
     QApplication app(argc,argv);
     QWidget w;
     w.setWindowTitle("主要看气质");  //设置标题
-    w.show();
 
     QPushButton b;
     b.setText("^_^");   //给按钮设置内容,对象和对象(窗口和窗口)没有关系,相互独立
-    b.show();
+//    b.show();
+    b.setParent(&w);
+    w.show();
+    /*如果不指定父对象,对象和对象(窗口和窗口)没有关系,相互独立
+     * a指定b为它的父对象, a放在b的上面
+     * 指定父对象,有2种方式:
+     *      1)setParent
+     *      2)通过构造函数传参
+     *  指定父对象,只需要父对象显示,上面的子对象自动显示
+    */
+
     app.exec();
     return 0;
 }

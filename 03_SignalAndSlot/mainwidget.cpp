0b844dcbae0864113513c7afbaacaeeee9d4c152
Fowindy <47002339@qq.com>
2020-11-12 22:25:09 +0800

    [重要]信号和槽

--- 03_SignalAndSlot/mainwidget.cpp
+++ 03_SignalAndSlot/mainwidget.cpp
@@ -4,12 +4,20 @@ MainWidget::MainWidget(QWidget *parent)
     : QWidget(parent)
 {
     b.setParent(this);
-    b.setText("^_^");
+    b.setText("关闭");
     b.move(100,100);
 
     //b1为指针,需要动态分配空间
     b1 = new QPushButton(this);
     b1->setText("ABC");
+
+    connect(&b,&QPushButton::pressed,this,&MainWidget::close);
+    /*
+     * &b:信号发出者,指针类型
+     * &QPushButton::pressed:处理信号   &发送者的类名::信号名字
+     * this:信号接收者
+     * &MainWidget::close:槽函数,信号处理函数    &接收者的类名::槽函数名字
+*/
 }
 
 MainWidget::~MainWidget()

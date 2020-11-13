ad75c9d397f5b8240bb17a51e820132fec39e672
Fowindy <47002339@qq.com>
2020-11-13 12:48:37 +0800

    按钮和槽函数还有信号的分工细节

--- 03_SignalAndSlot/subwidget.cpp
+++ 03_SignalAndSlot/subwidget.cpp
@@ -13,11 +13,15 @@ SubWidget::SubWidget(QWidget *parent) :
 	sb.move(50, 50);
 
 	//子窗体按钮触发
+	//按钮按下只是调用槽函数的功能,槽函数做什么跟按钮没关系
+	//按钮的作用:只是触发槽函数
+	//槽函数(回调函数):再来发信号
 	connect(&sb, &QPushButton::clicked, this, &SubWidget::SendSlot);
 }
 void SubWidget::SendSlot()
 {
 	//按下按钮,发送信号
+	//信号谁的,就由谁发出来
 	emit mySignal();
 	emit mySignal(250, "我是子窗口");
 }

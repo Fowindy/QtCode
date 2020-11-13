c6599dbe2b551f39ebb2474d2bf64922a446416e
Fowindy <47002339@qq.com>
2020-11-13 13:41:34 +0800

    对于父窗口(主窗口),坐标系统相当于屏幕

--- 04_QT_Coordinate/MyWidget.cpp
+++ 04_QT_Coordinate/MyWidget.cpp
@@ -4,4 +4,12 @@
 MyWidget::MyWidget(QWidget *parent)
 	: QWidget(parent)
 {
+	//因为move是从QWidget继承过来的,所以按钮和窗口都可以使用
+	/**
+	 * 对于父窗口(主窗口),坐标系统相当于屏幕
+	 原点:相对于屏幕左下角
+	 x:往右递增
+	 y:往左递增
+	 */
+	move(100, 100);
 }

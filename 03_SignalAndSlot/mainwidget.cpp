fb18e99e6ef93042dd56c8195727e2a88bbb44fb
Fowindy <47002339@qq.com>
2020-11-13 11:21:31 +0800

    Lamda表达式结构实现qDebug打印

--- 03_SignalAndSlot/mainwidget.cpp
+++ 03_SignalAndSlot/mainwidget.cpp
@@ -65,6 +65,19 @@ MainWidget::MainWidget(QWidget *parent)
     connect(&sw,SIGNAL(mySignal()),this,SLOT(dealSub()));
     connect(&sw,SIGNAL(mySignal(int,QString)),this,SLOT(DealSlot(int,QString)));
     //SIGNAL    SLOT 将函数名字 -> 字符串 不进行错误检查
+
+    //Lamda表达式,匿名函数对象
+    //C++11增加的新特性,项目文件: CONFIG += C++11;
+    //QT配合信号一起使用,非常方便
+
+    QPushButton *b4 = new QPushButton(this);
+    b4->setText("Lamda表达式");
+    b4->move(150,150);
+    connect(b4,&QPushButton::released,this,
+            []()
+    {
+        qDebug() << "11111111";
+    });
 }
 
 MainWidget::~MainWidget()
